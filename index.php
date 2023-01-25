75a25c162010ad6654843cccdf219514d4164f74
wajima <goo.hiro.w@gmail.com>
2023-01-25 13:07:24 +0900

    initial commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+
+
+
+
+
+// ----- 連想配列 -----
+// $arrray_member = [
+//   'name' => '本田',
+//   'height' => 180,
+//   'hobby' => 'サッカー'
+// ];
+
+// echo $arrray_member['hobby'];
+
+// echo '<pre>';
+// var_dump($arrray_member);
+// echo '</pre>';
+
+// $arrray_member2 = [
+//   '本田' => [
+//     'height' => 180,
+//     'hobby' => 'サッカー'
+//   ],
+//   '香川' => [
+//     'height' => 170,
+//     'hobby' => 'サッカー'
+//   ]
+// ];
+
+// echo $arrray_member2['香川']['height'];
+
+// echo '<pre>';
+// var_dump($arrray_member2);
+// echo '</pre>';
+
+
+// ----- 配列 -----
+// $array = [1, 2, 3];
+
+// echo $array[0] . '<br>';
+
+// echo '<pre>';
+// var_dump($array);
+// echo '</pre>';
+
+// $array2 = [
+//   ['hoge1', 'hoge2', 'hoge3'], 
+//   ['ほげ１', 'ほげ２', 'ほげ３']
+// ];
+
+// echo '<pre>';
+// var_dump($array2);
+// echo '</pre>';
+
+// echo $array2[1][1];
+
+
+// ----- 変数 -----
+// $test = 123;
+// const MAX = 'テスト';
+
+// echo $test . '<br>' . MAX;
+
+// var_dump(MAX);
