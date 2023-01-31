fb26db26b324cd570e69e2be8ea42aedf9cf2046
wajima <goo.hiro.w@gmail.com>
2023-01-31 12:45:57 +0900

    composerライブラリ追加 Carbon

--- composer_test/index.php
+++ composer_test/index.php
@@ -6,3 +6,9 @@ use App\Controllers\TestController;
 
 $app = new TestController;
 $app->run();
+
+use Carbon\Carbon;
+echo '<br>';
+echo Carbon::now();
+echo '<br>';
+echo Carbon::now()->format('y年m月d日');
